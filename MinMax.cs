ddd9c4689b1c5cccb32ef6bc6f77bacad0db3088
SariAzulay <85225021+SariAzulay@users.noreply.github.com>
2021-10-27 14:04:30 +0300

    Add files via upload

---
+++ MinMax.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    namespace ConsoleApp2
+    {
+        class MinMax
+        {
+            //If the array was not a fixed value I would use mergeSort
+            public static string MinMaxSum(int[] arr)
+            {
+                if (arr.Length == 0)
+                    return "0 0";
+                long MinMax = 0;
+                int max = 0;
+                int min = 0;
+                for (int i = 0; i < 5; i++)
+                {
+                    MinMax += arr[i];
+                    if (i == 0)
+                    {
+                        max = arr[i];
+                        min = arr[i];
+                    }
+                    else
+                    {
+                        if (arr[i] > max)
+                            max = arr[i];
+                        else
+                        {
+                            if (arr[i] < min)
+                                min = arr[i];
+                        }
+                    }
+                }
+                return (MinMax - max).ToString() + " " + (MinMax - min).ToString();
+
+            }
+
+
+            static void Main(string[] args)
+            {
+                int[] arr = { 1, 3, 5, 7, 9 };
+                Console.WriteLine(MinMaxSum(arr));
+            }
+
+
+        }
+    }
+
+}
---
+++ RecursiveDigitSum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class RecursiveDigitSum
+    {
+
+        public static int SuperDigit(string n, int k)
+        {
+            if (n.Length == 1)
+                return int.Parse(n);
+            int sum = 0;
+            for (int i = 0; i < n.Length; i++)
+            {
+                sum += int.Parse(n.Substring(i, 1));
+            }
+            if (k != 0)
+            {
+                sum = sum * k;
+                k = 0;
+            }
+            return SuperDigit(n = sum.ToString(), 0);
+
+        }
+        static void Main(string[] args)
+        {
+            int k = 4;
+            string s = "985";
+            Console.WriteLine(SuperDigit(s, k));
+        }
+    }
+}
+
